5f3aa3bdb348bd9536e815ca0cc734934ebd7ebb
Szymon Łukaszyk <szymon@patent.pl>
2025-08-31 20:39:21 +0200

    Add files via upload

--- cbcstates.m
+++ cbcstates.m
@@ -1,15 +1,15 @@
 function cbc = cbcstates(n, c)
 % Calculate the distribution of common bit (CB) states that is n-qubit states that in the computational basis 
-% have 0 <= c <= n-1 bit(s) in common in the same position(s) as a dunction of the support size
+% have 0 <= c <= n-1 bit(s) in common in the same position(s) as a dunction of the support size (k)
 % Input: 
 % n - number of qubits
 % c - number of common bit(s)
 % For c=0 returns the number of inseparable states
-% For c=1 returns the number of all-partition separable states
+% For c=1 returns the number of all-partition separable states if k=2
 %
 % E.g. cbc = cbcstates(3, 0)
-% spsize =  2 3  4  5  6  7 8
-% cbc    =  4 32 64 56 28 8 1
+% k   =  2  3  4  5  6  7  8
+% cbc =  4 32 64 56 28  8  1
 %
 % (c) Szymon Lukaszyk
 % licensed under MIT License
@@ -25,17 +25,11 @@ if c < 0 | c > n-1
   error 'c must be 0 <= c <= n-1 in cbcstates.';
 end
 
-fct = mynchoosek(n, c)*2^c;
 k=2:2^(n-c);
 
+fct = mynchoosek(n, c)*2^c;
 for kk=1:length(k)
-    msum = 0;
-    for m=0:n-c
-        prod = (-1)^m * mynchoosek(n-c, m) * 2^m * mynchoosek(2^(n-c-m), k(kk));
-        msum = msum + prod;
-    end
-    cbc(kk) = msum*fct;
+    cbc(kk) = cbcstate(n, c, k(kk));
 end
 
 end
-
